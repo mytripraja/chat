e71ead6b013454289e5fadc1d37939f08a1d4654
mytripraja <mytripraja@gmail.com>
2026-08-11 22:24:20 +0530

    first commit: separated tourism planner apps

---
+++ src/lib/appwrite.js
@@ -0,0 +1,14 @@
+import { Client, Account, Databases } from 'appwrite';
+
+const client = new Client()
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1')
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+
+export const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+export const COLLECTIONS = {
+  CHATS: 'chats',
+  MESSAGES: 'messages',
+};
